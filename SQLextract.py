8d50332effef17bc52b1dc35fc7f906b8a6ac4f8
RDBinns <rdpbinns@gmail.com>
2016-07-04 17:42:47 +0100

    Update SQLextract.py

--- SQLextract.py
+++ SQLextract.py
@@ -23,12 +23,3 @@ for x in range (1, 1000): # restricted to the
     with open("data3.csv", "a") as data:
         data.write(str(row))
 db.close
-	
-Click here to Reply or Forward
-8.42 GB (49%) of 17 GB used
-Manage
-Terms - Privacy
-Last account activity: 0 minutes ago
-Currently being used in 1 other location  Details
-
-
